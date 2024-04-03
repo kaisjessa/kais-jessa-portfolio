fbda8a5ed8ee92ee1e499207f52b35fa3675cacc
Kais Jessa <kaisjessa@gmail.com>
2024-04-03 16:23:17 -0400

    added dailyui theme controller

---
+++ app/components/footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="footer items-center p-4 bg-base-300 text-base-content">
+      <aside className="items-center grid-flow-col">
+        <p>Kais Jessa</p>
+      </aside>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ app/components/header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div>
+      <input type="checkbox" value="dark" className="toggle theme-controller" />
+    </div>
+  );
+};
+
+export default Header;
